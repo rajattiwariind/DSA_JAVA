805490480847758ff7135881e0cc2da624f08c1e
rajattiwariind <129544468+rajattiwariind@users.noreply.github.com>
2023-04-03 14:58:09 +0530

    Create SumOfProduct.java

---
+++ JAVA_BASIC/SumOfProduct.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class sumofproduct {
+        public static void main(String[]args) {
+            Scanner sc = new Scanner(System.in);
+            int k = sc.nextInt();
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            int c = sc.nextInt();
+            int output =a*k+b*k+c*k;
+            System.out.println(output);
+            }
+        }

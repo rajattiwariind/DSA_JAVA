d52255514b28153eff75d73741d5600775374f85
rajattiwariind <129544468+rajattiwariind@users.noreply.github.com>
2023-04-07 21:40:31 +0530

    Create whileloop.java

---
+++ JAVA_BASIC/whileloop.java
@@ -0,0 +1,10 @@
+public class whileloop {
+    public static void main(String[]args){
+
+        int i=1;
+        while(i<8){
+            System.out.println(i);
+            i++;
+        }
+    }
+}

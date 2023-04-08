b666ac80eb9e126fdce1f50f20e36122649eec5b
rajattiwariind <129544468+rajattiwariind@users.noreply.github.com>
2023-04-08 14:00:07 +0530

    Create BreakTheLoop.java

---
+++ JAVA_BASIC/BreakTheLoop.java
@@ -0,0 +1,11 @@
+public class BreakTheLoop {
+    public static void main(String[]args){
+
+        for( int i=0; i<5; i++) {
+
+            for(int j=0; j<4; j++){
+                System.out.println( " i = "+ i + " j = " +j);
+            }
+        }
+    }
+}

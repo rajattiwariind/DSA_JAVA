c3128e535d40a526ca89664ce61087079116d19d
rajattiwariind <129544468+rajattiwariind@users.noreply.github.com>
2023-04-09 14:48:22 +0530

    Create PrintMasaiSchool.java

---
+++ JAVA_BASIC/PrintMasaiSchool.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class PrintMasaiSchool {
+    public static void main(String[]args){
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("Enter the number");
+        int num=scanner.nextInt();
+        for(int i=1; i<=num; i++){
+            System.out.println("Masai School");
+        }
+    }
+}

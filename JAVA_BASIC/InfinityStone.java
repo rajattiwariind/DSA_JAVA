8efe2c55e1538abe97cb5fb9d97175e25b5e3be4
rajattiwariind <129544468+rajattiwariind@users.noreply.github.com>
2023-04-09 12:03:54 +0530

    Create InfinityStone.java

---
+++ JAVA_BASIC/InfinityStone.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class InfinityStone {
+    public static void main(String[]args){
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("Enter the number");
+        int a=scanner.nextInt();
+        int b=scanner.nextInt();
+        int c=scanner.nextInt();
+        int d=scanner.nextInt();
+        int e=scanner.nextInt();
+        int f=scanner.nextInt();
+        System.out.println(a*1+b*2+c*3+d*3+e*2+f*2);
+    }
+}

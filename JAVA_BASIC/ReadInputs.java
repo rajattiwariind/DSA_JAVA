3a9d158d43dbdf20c068e6214ce2e6f95e4feb41
rajattiwariind <129544468+rajattiwariind@users.noreply.github.com>
2023-04-11 17:50:50 +0530

    Create ReadInputs.java

---
+++ JAVA_BASIC/ReadInputs.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class readsinput {
+        public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Enter the  first number");
+            long number = scanner.nextLong();
+            System.out.println(number);
+
+
+        }
+    }

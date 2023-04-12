5edd6cbfc4b00a7eeaa7a9927221d792f47d53fc
rajattiwariind <129544468+rajattiwariind@users.noreply.github.com>
2023-04-12 16:46:33 +0530

    Create Greater.java

---
+++ JAVA_BASIC/Greater.java
@@ -0,0 +1,15 @@
+//Given two numbers A and B print which is greater//
+public class Greater {
+    public static void main(String[]args){
+        int a=10;
+        int b=20;
+
+        if(a>b){
+            System.out.println("a is greater");
+        } else if (b>a) {
+            System.out.println("b  is greater");
+
+        }
+    }
+
+}

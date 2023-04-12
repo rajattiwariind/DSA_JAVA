aa9647dfc1f5bc2023752509b58d5759cdbd0f43
rajattiwariind <129544468+rajattiwariind@users.noreply.github.com>
2023-04-12 14:32:20 +0530

    Create FindX.java

---
+++ JAVA_BASIC/FindX.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class FindX {
+    public static void main(String[]args){
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("Enter the number");
+        int x=scanner.nextInt();
+        System.out.println(x*3+10);
+    }
+}

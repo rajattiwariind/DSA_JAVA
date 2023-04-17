263c0d077f8b1da70838aea7892bb74bb14b376d
rajattiwariind <129544468+rajattiwariind@users.noreply.github.com>
2023-04-17 14:09:35 +0530

    Create SquareIt.java

---
+++ JAVA_BASIC/SquareIt.java
@@ -0,0 +1,8 @@
+public class SquareIt {
+    public static void main(String[]args){
+
+         for (int i = 1; i <= 10; i++){
+            System.out.println(" The Square of " + i +" is " +(i*i) );
+        }
+    }
+}

3c7fba602693d45c225bd49bb489d7c64284fc96
rajattiwariind <129544468+rajattiwariind@users.noreply.github.com>
2023-04-17 14:08:57 +0530

    Create SquareAndSum.java

---
+++ JAVA_BASIC/SquareAndSum.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class SquareAndSum {
+    public static void main(String[]args){
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("Enter the number");
+        int a=scanner.nextInt();
+        int b=scanner.nextInt();
+        int c=scanner.nextInt();
+        System.out.println(a*1+b*2+c*3);
+    }
+}

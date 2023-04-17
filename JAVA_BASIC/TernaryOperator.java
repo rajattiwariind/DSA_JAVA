eac601c3ae8d5174c28b4acdc8032e49e5a568c3
rajattiwariind <129544468+rajattiwariind@users.noreply.github.com>
2023-04-17 14:12:32 +0530

    Create TernaryOperator.java

---
+++ JAVA_BASIC/TernaryOperator.java
@@ -0,0 +1,10 @@
+//Given two numbers A and B print which is smaller (Use ternary operator)//
+public class TernaryOperator {
+    public static void main(String[]args){
+        int a=6;
+        int b=5;
+        String result =(a<b) ? "a is less than b":"b is less than a";
+        System.out.println(result);
+    }
+
+}

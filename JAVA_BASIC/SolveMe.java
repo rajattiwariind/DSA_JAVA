56da4a6f1f3540f799af7a2f0fd030e9ac14a55b
rajattiwariind <129544468+rajattiwariind@users.noreply.github.com>
2023-04-17 14:07:40 +0530

    Create SolveMe.java

---
+++ JAVA_BASIC/SolveMe.java
@@ -0,0 +1,16 @@
+public class SolveMe {
+    public static void main(String[]args){
+
+        int i = 50 ;
+
+        while (i <= 150){
+
+            if(i % 13 == 0){
+                System.out.println(i);
+            }
+            i++;
+
+        }
+
+    }
+}

1650142ac5ebe7ea894152cc264091dfb13da645
Rajat Tiwari <129544468+rajattiwariind@users.noreply.github.com>
2023-04-18 20:02:29 +0530

    Create DoubleAll.java

---
+++ JAVA_BASIC/DoubleAll.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class DoubleAll {
+    public static void main(String[]args){
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("Enter the number");
+        int a=scanner.nextInt();
+        int b=scanner.nextInt();
+        int c=scanner.nextInt();
+        int d=scanner.nextInt();
+        int e=scanner.nextInt();
+        System.out.println(a*2+b*2+c*2+d*2+e*2);
+    }
+}

4e5105a7acbc2fd594f2d0c6f948c95611ef1139
Rajat Tiwari <129544468+rajattiwariind@users.noreply.github.com>
2023-04-18 19:52:29 +0530

    Create Cousins.java

---
+++ JAVA_BASIC/Cousins.java
@@ -0,0 +1,12 @@
+public class Cousins {
+    public static void main(String[]args){
+
+        for (int i = 50 ; i <= 150 ; i++){
+
+            if(i % 7 ==0){
+                System.out.println(i);
+            }
+
+        }
+    }
+}

460dbf73b6ad50bef0dc2cc29e92310b654d0cf5
Rajat Tiwari <129544468+rajattiwariind@users.noreply.github.com>
2023-04-20 12:27:11 +0530

    Create TotalIncome.java

---
+++ JAVA_BASIC/TotalIncome.java
@@ -0,0 +1,9 @@
+public class TotalIncome {
+    public static void main(String[] args){
+        double gym=2000.50;
+        double teaching=15000.750;
+        double Classes=3000;
+        double TotalIncome=gym+teaching+Classes;
+        System.out.println("TotalIncome"+TotalIncome);
+    }
+}

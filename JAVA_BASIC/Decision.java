8dea1990acff61305dbb8780a5aaf2d131035ed2
Rajat Tiwari <129544468+rajattiwariind@users.noreply.github.com>
2023-04-21 12:14:10 +0530

    Create Decision.java

---
+++ JAVA_BASIC/Decision.java
@@ -0,0 +1,12 @@
+public class Decision {
+    public static void main(String[] args){
+        int num=5;
+
+        if (num % 2==0){
+            System.out.println("num is even");
+        }
+        else{
+            System.out.println("num is odd");
+        }
+    }
+}

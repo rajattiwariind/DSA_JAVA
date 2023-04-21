f32ba096a580cfd0877b354584ceafd28b7f796d
Rajat Tiwari <129544468+rajattiwariind@users.noreply.github.com>
2023-04-21 12:17:13 +0530

    Create Double.java

---
+++ JAVA_BASIC/Double.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Double {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        int d = sc.nextInt();
+        int e = sc.nextInt();
+        int output=a*2+b*2+c*2+d*2+e*2;
+        System.out.println("Sum of double number"+output);
+
+
+    }
+}

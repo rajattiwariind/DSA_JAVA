7d42678f435f5ada9dca701a216b3ab770512d07
Rajat Tiwari <129544468+rajattiwariind@users.noreply.github.com>
2023-04-21 12:17:50 +0530

    Create DoubleTwoTripleOne.java

---
+++ JAVA_BASIC/DoubleTwoTripleOne.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class DoubleTwoTripleOne {
+    public static void main(String[]args){
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("Enter the number");
+        int a=scanner.nextInt();
+        int b=scanner.nextInt();
+        int c=scanner.nextInt();
+        int d=scanner.nextInt();
+        System.out.println(a+1+b+2+c+6+d+4);
+    }
+}

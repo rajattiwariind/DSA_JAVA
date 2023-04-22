6f9c30214cddb9436a66f617a905f1543fd6f005
Rajat Tiwari <129544468+rajattiwariind@users.noreply.github.com>
2023-04-22 21:50:12 +0530

    Create Number.java

---
+++ JAVA_BASIC/Number.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Number {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a and b number:");
+        int a = sc.nextInt();
+        int b= sc.nextInt();
+        if(a<b){
+            System.out.println("a is smaller than b");
+        }
+        if(a>b)
+            System.out.println("a is greater than b");
+        }
+
+    }

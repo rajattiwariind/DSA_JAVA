ff5110999a8daf148566b49a018d12dd5adc289b
Rajat Tiwari <129544468+rajattiwariind@users.noreply.github.com>
2023-04-22 11:53:20 +0530

    Create TakingInputs.java

---
+++ JAVA_BASIC/TakingInputs.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class TakingInputs {
+
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            int n = sc.nextInt(); // n is the input
+        }
+    }
+
+

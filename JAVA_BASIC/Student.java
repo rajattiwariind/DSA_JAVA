634448f617296dd2fd89fd37a7a488e70dc2a39b
Rajat Tiwari <129544468+rajattiwariind@users.noreply.github.com>
2023-04-22 11:56:25 +0530

    Create Student.java

---
+++ JAVA_BASIC/Student.java
@@ -0,0 +1,7 @@
+public class Student {
+    public static void main(String[] args){
+        double noOfChocolate=27;
+        double eachStudent=noOfChocolate%5;
+        System.out.println("Number of student left behind is" +eachStudent);
+    }
+}

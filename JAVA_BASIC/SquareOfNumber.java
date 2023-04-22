bc6a7073697a26a07ea8de2a2c84ba25153b9634
Rajat Tiwari <129544468+rajattiwariind@users.noreply.github.com>
2023-04-22 11:54:56 +0530

    Create SquareOfNumber.java

---
+++ JAVA_BASIC/SquareOfNumber.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class SquareOfNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int result = a * a;
+        System.out.println("The Square of number is:" + result);
+    }
+}

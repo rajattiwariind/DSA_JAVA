c6c9ff1ced103faae86967d4faea9e2d9b167e61
Rajat Tiwari <129544468+rajattiwariind@users.noreply.github.com>
2023-04-22 11:51:20 +0530

    Create WhileSquareIt.java

---
+++ JAVA_BASIC/WhileSquareIt.java
@@ -0,0 +1,9 @@
+public class WhileSquareIt {
+    public static void main(String[]args){
+        int i=1;
+        while (i<=10){
+            System.out.println(" The Square of " + i +" is " +(i*i));
+            i++;
+        }
+    }
+}

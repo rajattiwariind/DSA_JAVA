1994be9d54d666f36791be3f8ea6f4e71405d97f
Rajat Tiwari <129544468+rajattiwariind@users.noreply.github.com>
2023-04-22 11:58:06 +0530

    Create SumOfFive.java

---
+++ JAVA_BASIC/SumOfFive.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class SumofFive {
+    public static void main(String[]args){
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("enter the number");
+        int a=scanner.nextInt();
+        int b=scanner.nextInt();
+        int c=scanner.nextInt();
+        int d=scanner.nextInt();
+        int e=scanner.nextInt();
+        System.out.println(a+b+c+d+e);
+    }
+}

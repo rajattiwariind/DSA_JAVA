60497c834f3c50e9be29f4c3cd736c0fc05ca4f1
Rajat Tiwari <129544468+rajattiwariind@users.noreply.github.com>
2023-04-22 11:59:24 +0530

    Create SumOfVariable.java

---
+++ JAVA_BASIC/SumOfVariable.java
@@ -0,0 +1,9 @@
+public class SumOfVariable {
+    public static void main(String[]args){
+        int a=12;
+        int b=45;
+        int c=23;
+        int output=a+b+c;
+        System.out.println("Sum Of Three Variable:" + output);
+    }
+}

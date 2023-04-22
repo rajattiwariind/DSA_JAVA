9cf44f1da5a1debfee54e27adb8828a37e7c9c01
Rajat Tiwari <129544468+rajattiwariind@users.noreply.github.com>
2023-04-22 11:58:44 +0530

    Create SumOfString.java

---
+++ JAVA_BASIC/SumOfString.java
@@ -0,0 +1,8 @@
+public class SumOfString {
+    public static void main(String[]args){
+        String a ="42";
+        String b ="25";
+        int output=Integer.parseInt(a)+Integer.parseInt(b);
+        System.out.println(output);
+    }
+}

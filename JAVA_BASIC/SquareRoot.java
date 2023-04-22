71b83c56e2055630f51b20d188647540f79cd18c
Rajat Tiwari <129544468+rajattiwariind@users.noreply.github.com>
2023-04-22 11:55:38 +0530

    Create SquareRoot.java

---
+++ JAVA_BASIC/SquareRoot.java
@@ -0,0 +1,8 @@
+public class SquareRoot {
+    public static void main(String []args){
+        double number=5;
+        double SquareRoot=Math.sqrt(number);
+        System.out.println(SquareRoot);
+
+    }
+}

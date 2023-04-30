fe52188ed0ea1caab40fd0b98f94647afcd06008
Rajat Tiwari <129544468+rajattiwariind@users.noreply.github.com>
2023-04-30 12:57:15 +0530

    Create ArithmeticOperator.java

---
+++ JAVA_BASIC/ArithmeticOperator.java
@@ -0,0 +1,9 @@
+public class ArithmeticOperator {
+    public static void main(String[]args){
+        float a=10.001f;
+        int b=20;
+        float sum=a+b;
+        System.out.println("the sum of a amd b is"+sum);
+
+    }
+}

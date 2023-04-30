a11855bb5e6aa36d1da18803acaebb25657abcf8
Rajat Tiwari <129544468+rajattiwariind@users.noreply.github.com>
2023-04-30 21:31:44 +0530

    Create LengthOfString.java

---
+++ JAVA_BASIC/LengthOfString.java
@@ -0,0 +1,7 @@
+public class LengthOfString {
+    public static void main(String[] args) {
+        String name = "Masai School";
+        int length = name.length();
+        System.out.println(length);
+    }
+}

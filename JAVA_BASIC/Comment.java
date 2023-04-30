eec56f4a21edd623deb0dc2fc67a99e8616e3e10
Rajat Tiwari <129544468+rajattiwariind@users.noreply.github.com>
2023-04-30 12:50:46 +0530

    Create Comment.java

---
+++ JAVA_BASIC/Comment.java
@@ -0,0 +1,7 @@
+public class Comment {
+    public static void main(String []args){
+        System.out.println("//This is a single-line comment");
+        System.out.println("/*This is a multi-line comment*/");
+    }
+
+}

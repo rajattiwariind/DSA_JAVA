8bac2921d81460537c17b7ef62fec980d53d46ed
Rajat Tiwari <129544468+rajattiwariind@users.noreply.github.com>
2023-04-30 12:56:29 +0530

    Create QuadraticEquation.java

---
+++ JAVA_BASIC/QuadraticEquation.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class quadraticequation {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int Output=n*n+2*n+1;
+        System.out.println(Output);
+    }
+}

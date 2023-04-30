eb108215e5a6bb31b3a437ca48413b14c09cad7b
Rajat Tiwari <129544468+rajattiwariind@users.noreply.github.com>
2023-04-30 12:55:22 +0530

    Create harryandmaghan.java

---
+++ harryandmaghan.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class harryandmaghan {
+    public static void main(String[]args) {
+        Scanner sc = new Scanner(System.in);
+        int n =sc.nextInt();
+        int Output=n*3+7-10;
+        System.out.println(Output);
+    }
+}

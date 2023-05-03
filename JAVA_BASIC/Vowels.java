897b08f766d1253ca2cc60a3d8d74ac902ff014d
Rajat Tiwari <129544468+rajattiwariind@users.noreply.github.com>
2023-05-03 22:01:05 +0530

    Create Vowels.java

---
+++ JAVA_BASIC/Vowels.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Vowels {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the size of the array:");
+        int n= sc.nextInt();
+        System.out.println("Enter the array element");
+        int[] num=new int[n];
+
+        for(int i=0; i<n; i++){
+           num[i]= sc.nextInt();
+        }
+       for(int i=0; i<n; i++){
+           System.out.println(num[i]);
+       }
+    }
+}

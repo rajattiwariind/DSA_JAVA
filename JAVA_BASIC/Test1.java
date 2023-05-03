de8c4c95709f477a61eda6610146cb1fbfd5ccf5
Rajat Tiwari <129544468+rajattiwariind@users.noreply.github.com>
2023-05-03 21:47:34 +0530

    Create Test1.java

---
+++ JAVA_BASIC/Test1.java
@@ -0,0 +1,10 @@
+import java.io.OptionalDataException;
+
+public class Test1 {
+    public static void main(String[]args){
+       int[]arr= {1,2,3,4,55};
+       for (int i=0; i<arr.length; i++ ){
+           System.out.println(arr[i]);
+       }
+    }
+}

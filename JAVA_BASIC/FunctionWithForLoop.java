96d71c881baac4a41d4ee6f1f2ffc4b19d227278
Rajat Tiwari <129544468+rajattiwariind@users.noreply.github.com>
2023-05-07 20:45:12 +0530

    Create FunctionWithForLoop.java

---
+++ JAVA_BASIC/FunctionWithForLoop.java
@@ -0,0 +1,14 @@
+public class Functions {
+    public static void main(String []args){
+        for(int i=1;i<=10;i++)
+        {
+
+            if(i==6)
+            {
+                return;
+            }
+            System.out.println(i);
+        }
+    }
+
+}

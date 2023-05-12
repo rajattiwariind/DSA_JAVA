cdc3b8df28a1ad973278e4186eff9da1b8779b0c
Rajat Tiwari <129544468+rajattiwariind@users.noreply.github.com>
2023-05-12 21:29:17 +0530

    Create threeDimen1.java

---
+++ JAVA_BASIC/threeDimen1.java
@@ -0,0 +1,40 @@
+public class threeDimen {
+    public static void main(String[] args) {
+        int[][][] threeDimen = {
+                {
+                        {
+                                10, 20, 30
+                        },                              //Electronics Department
+                        {
+                                20, 40, 80
+                        }
+                },
+                {
+                        {
+                                10, 25, 65
+                        },                            // CS Department
+                        {
+                                60, 70, 80
+                        }
+                },
+                {
+                        {
+                                10, 21, 55
+                        },
+                        {                              //Mac Department
+                                62, 40, 10
+                        }
+                }
+
+        };
+
+        for (int depart = 0; depart < 3; depart++) {
+            for (int students = 0; students < 2; students++) {
+                for (int marks = 0; marks < 3; marks++) {
+                    System.out.print(threeDimen[depart][students][marks] + " ");
+                }
+                System.out.println();
+            }
+        }
+    }
+}

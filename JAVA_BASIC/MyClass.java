d9ef10e2f7fb09d3eb555eba8ed1a0343b95d0d1
Rajat Tiwari <129544468+rajattiwariind@users.noreply.github.com>
2023-05-13 22:02:49 +0530

    Create MyClass.java

---
+++ JAVA_BASIC/MyClass.java
@@ -0,0 +1,9 @@
+public class MyClass {
+    int x;
+
+    public static void main(String[] args) {
+        MyClass myObj = new MyClass();
+        myObj.x = 40;
+        System.out.println(myObj.x);
+    }
+}

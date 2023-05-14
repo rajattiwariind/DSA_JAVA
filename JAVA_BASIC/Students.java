30c870a0079a153d83a75e00954c526458964fef
Rajat Tiwari <129544468+rajattiwariind@users.noreply.github.com>
2023-05-14 17:14:22 +0530

    Create Students.java

---
+++ JAVA_BASIC/Students.java
@@ -0,0 +1,14 @@
+public class Students {
+    String name;
+    int age;
+    String address;
+    public static void main(String []args){
+        Students student=new Students();
+        student.name="Krishna";
+        student.age=20;
+        student.address="4th floor ABC";
+        System.out.println(student.name);
+        System.out.println(student.age);
+        System.out.println(student.address);
+    }
+}

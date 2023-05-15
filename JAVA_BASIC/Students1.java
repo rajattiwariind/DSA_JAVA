6e0fb16000eaa08e075ca142b6c98f9d4f5c105e
Rajat Tiwari <129544468+rajattiwariind@users.noreply.github.com>
2023-05-15 13:47:24 +0530

    Create Students1.java

---
+++ JAVA_BASIC/Students1.java
@@ -0,0 +1,13 @@
+public class Students1 { 
+    public static void main(String[]args){
+        Students students = new Students();
+        students.name="Rajat";
+        students.age=17;
+        students.address="Mumbai";
+
+        System.out.println("Name is " + students.name);
+        System.out.println("Age is " + students.age);
+        System.out.println("Address is " + students.address);
+    }
+
+}

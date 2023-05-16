f6fa57d08fdab611072ccf4b2ddf852885eef30e
Rajat Tiwari <129544468+rajattiwariind@users.noreply.github.com>
2023-05-16 18:12:04 +0530

    Create Square using Constructor.java

---
+++ JAVA_BASIC/Square using Constructor.java
@@ -0,0 +1,22 @@
+public class Square {
+    int side;
+
+    public Square(int side) {
+        this.side = side;
+    }
+    public static void main(String[] args){
+        Square square=new Square( 4);
+
+    square.calculateArea();
+    square.calculatePerimeter();
+
+    }
+    public void calculateArea(){
+        int area=side*side;
+        System.out.println("Area of the square is "+area);
+    }
+    public void calculatePerimeter(){
+        int perimeter=4*side;
+        System.out.println("Perimeter of the square is "+perimeter);
+    }
+}

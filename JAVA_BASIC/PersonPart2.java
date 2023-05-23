d3d10597bad594c1d3861fcb55982d4be3bf13f6
Rajat Tiwari <129544468+rajattiwariind@users.noreply.github.com>
2023-05-23 18:32:53 +0530

    Create PersonPart2.java

---
+++ JAVA_BASIC/PersonPart2.java
@@ -0,0 +1,9 @@
+public class PersonTestDrive {
+    public static void main(String [] args){
+        Person person = new Person("Rajat",17,'M');
+        System.out.println(person.name);
+        System.out.println(person.age);
+        System.out.println(person.gender);
+
+    }
+}

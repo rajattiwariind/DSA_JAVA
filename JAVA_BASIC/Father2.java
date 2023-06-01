5b638c53c4c19858911059eda4df4c78e9959113
Rajat Tiwari <129544468+rajattiwariind@users.noreply.github.com>
2023-06-01 12:01:32 +0530

    Create Father2.java

---
+++ JAVA_BASIC/Father2.java
@@ -0,0 +1,8 @@
+public class Driver {
+    public static void main(String[]args){
+       Father father = new Father("Masai",2);
+        System.out.println(father.getName());
+        System.out.println(father.getAge());
+
+    }
+}

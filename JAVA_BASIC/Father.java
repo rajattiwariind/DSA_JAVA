a4f9e5c81736503aea1254dc97b66a84566901c2
Rajat Tiwari <129544468+rajattiwariind@users.noreply.github.com>
2023-06-01 12:01:00 +0530

    Create Father.java

---
+++ JAVA_BASIC/Father.java
@@ -0,0 +1,16 @@
+public class Father {
+
+    public String name;
+    public int age;
+
+    public Father(String name,int age){
+        this.name = name;
+        this.age = age;
+    }
+    public String getName(){
+        return name;
+    }
+    public int getAge(){
+        return age;
+    }
+}

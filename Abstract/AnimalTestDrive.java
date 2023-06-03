e14c36cbfe141ed075722c3e11638f8034f8f951
Rajat Tiwari <129544468+rajattiwariind@users.noreply.github.com>
2023-06-03 11:37:15 +0530

    Create AnimalTestDrive.java

---
+++ Abstract/AnimalTestDrive.java
@@ -0,0 +1,10 @@
+public class AnimalTestDrive {
+    public static void main(String[]args){
+        Dog dog = new Dog();
+        dog.speak();
+        dog.category();
+        dog.AreDogsLoyal();
+        Animal cat = new Cat();
+        cat.speak();
+    }
+}

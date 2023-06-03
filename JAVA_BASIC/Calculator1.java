0d8db688864d79617b2dc111ee8109a170ca1954
Rajat Tiwari <129544468+rajattiwariind@users.noreply.github.com>
2023-06-03 09:47:43 +0530

    Create Calculator1.java

---
+++ JAVA_BASIC/Calculator1.java
@@ -0,0 +1,10 @@
+public class Calculator {
+
+    public int add(float a, float b) {
+        return (int) (a + b);
+    }
+
+    public int add(int a, int b, int c) {
+        return a + b + c;
+    }
+}

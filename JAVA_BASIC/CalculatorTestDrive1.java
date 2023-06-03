706bcd326c73ffae96fb5355278241345f77e0da
Rajat Tiwari <129544468+rajattiwariind@users.noreply.github.com>
2023-06-03 09:49:17 +0530

    Create CalculatorTestDrive1.java

---
+++ JAVA_BASIC/CalculatorTestDrive1.java
@@ -0,0 +1,7 @@
+public class CalculatorTestDrive {
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+        System.out.println(calculator.add(2.2f, 3.344f));
+        System.out.println(calculator.add(2, 3, 5));
+    }
+}

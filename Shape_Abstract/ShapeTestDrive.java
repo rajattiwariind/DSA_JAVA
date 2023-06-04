7173dc2b335f9006d5b16ffa8a759902245c4d8f
Rajat Tiwari <129544468+rajattiwariind@users.noreply.github.com>
2023-06-04 12:22:08 +0530

    Create ShapeTestDrive.java

---
+++ Shape_Abstract/ShapeTestDrive.java
@@ -0,0 +1,9 @@
+public class ShapeTestDrive {
+    public static void main(String []args){
+        Square square = new Square(4);
+        square.calculateArea();
+
+        Rectangle rectangle = new Rectangle(2 , 4);
+        rectangle.calculateArea();
+    }
+}

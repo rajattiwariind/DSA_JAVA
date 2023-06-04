e1ab289ec339a43d6496cd7fd3a169aeacc2f3ab
Rajat Tiwari <129544468+rajattiwariind@users.noreply.github.com>
2023-06-04 11:53:11 +0530

    Create Calculator.java

---
+++ Calculator/Calculator.java
@@ -0,0 +1,13 @@
+public class Calculator {
+    public int add (int a , int b){
+        return a + b;
+    }
+    public double add(double a , double b){
+        return a + b;
+    }
+
+    public void area(int length , double breadth){
+        double area = length*breadth;
+        System.out.println("Area od rectangle is " + area);
+    }
+}

034b910a601033e9abbd9d9bf69bd9ad5594b24b
Rajat Tiwari <129544468+rajattiwariind@users.noreply.github.com>
2023-06-04 10:53:28 +0530

    Create Parent.java

---
+++ Polymorphism/Parent.java
@@ -0,0 +1,8 @@
+public class Parent {
+    public Parent(){
+        System.out.println("Parent class const");
+    }
+    protected void print(){
+        System.out.println("Parent class");
+    }
+}

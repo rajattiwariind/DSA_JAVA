81336f66eddf33a7ce72d1bfd4df8eceab04a918
Rajat Tiwari <129544468+rajattiwariind@users.noreply.github.com>
2023-06-09 19:42:24 +0530

    Create BruteForce.java

---
+++ JAVA_BASIC/BruteForce.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class BruteForce {
+    public static void main(String[] args){
+    String name="masai";
+    for(int i=1;i<=name.length();i++){
+        for(int j=1;j<name.length();j++){
+            System.out.println(name.charAt(i));
+             }
+        }
+    }
+}

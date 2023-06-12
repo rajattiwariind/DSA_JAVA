951838fca18a8ecad2027c9483d45bff75e59e85
Rajat Tiwari <129544468+rajattiwariind@users.noreply.github.com>
2023-06-12 19:50:06 +0530

    Create booleanIsActive.java

---
+++ JAVA_BASIC/booleanIsActive.java
@@ -0,0 +1,10 @@
+public static void add(int a, int b, boolean isActive)
+   { 
+      if(isActive)
+        {
+           return a+b;
+        } else {
+        
+           return 0;
+           }
+   }

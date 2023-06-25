fd06568fa38b9fc30b41ef85271203e1ae2a572a
Rajat Tiwari <129544468+rajattiwariind@users.noreply.github.com>
2023-06-25 18:21:13 +0530

    Create ListNameStrings.java

---
+++ JAVA_BASIC/ListNameStrings.java
@@ -0,0 +1,10 @@
+public class Strings {
+    public static void main(String[]args){
+       String name = "Rajat Ashu MasaiSchool";
+       String[] names = name.split(" ");
+       for(String Na : names){
+           System.out.println(Na);
+       }
+
+    }
+}

8f3dff429ef8dd7a5c9dcf9fd93ffda5952271cb
Rajat Tiwari <129544468+rajattiwariind@users.noreply.github.com>
2023-07-08 10:38:13 +0530

    Update ReverseArray.java

--- JAVA_BASIC/ReverseArray.java
+++ JAVA_BASIC/ReverseArray.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class ReverseArray {
+public class reverseArray {
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
         System.out.println("Enter the size of the array:");

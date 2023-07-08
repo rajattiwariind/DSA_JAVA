912707cb14a097ecd7af86f00c9ea095730878ff
Rajat Tiwari <129544468+rajattiwariind@users.noreply.github.com>
2023-07-08 10:48:08 +0530

    Create MasaiSchoolArray.java

---
+++ JAVA_BASIC/MasaiSchoolArray.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class MasaiSchoolArray {
+    public static void main(String[]args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the number");
+        int N = scanner.nextInt();
+        String Name = "MasaiSchool!";
+        int Names = Name.length();
+        System.out.println(Name);
+        }
+    }

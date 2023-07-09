fabd632274f5834dc3f8b1c5bb4b4f40580c2390
Rajat Tiwari <129544468+rajattiwariind@users.noreply.github.com>
2023-07-09 12:57:33 +0530

    Create  Operator3.java

---
+++ New Java/Operator3.java
@@ -0,0 +1,11 @@
+public class Operator{
+public static void main(String[] args){
+        int a=10;
+        int b=2;
+        System.out.println(a&b);
+        System.out.println(a|b);
+        System.out.println(a^b);
+        System.out.println(a<<b);
+        System.out.println(a>>b);
+        }
+        }

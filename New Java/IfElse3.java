106f6c314fc88e821357905e985c3e3cf247b6a5
Rajat Tiwari <129544468+rajattiwariind@users.noreply.github.com>
2023-07-11 11:21:19 +0530

    Create IfElse3.java

---
+++ New Java/IfElse3.java
@@ -0,0 +1,24 @@
+package com.masai;
+public class Main {
+public static void main(String[] args){
+int i=10;
+switch(i)
+{
+case 5:
+System.out.println("Five");
+break;
+case 10:
+System.out.println("Ten");
+break;
+case 15:
+System.out.println("Fifteen");
+break;
+case 20:
+System.out.println("Twenty");
+break;
+default:
+System.out.println("Default");
+break;
+}
+}
+}

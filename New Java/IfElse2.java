8ff42c0ca16d784506ddf2cb40af90408be091f7
Rajat Tiwari <129544468+rajattiwariind@users.noreply.github.com>
2023-07-11 11:15:47 +0530

    Create IfElse2.java

---
+++ New Java/IfElse2.java
@@ -0,0 +1,21 @@
+package com.masai;
+public class Main {
+public static void main(String[] args)
+{
+int i=10;
+int j;
+if(i==10)
+{
+j=20;
+}
+else if(i==20)
+{
+j=30;
+}
+else
+{
+j=40;
+}
+System.out.println(j);
+}
+}

0eb74839304cb561944b7984497a753c7a80ba2d
Rajat Tiwari <129544468+rajattiwariind@users.noreply.github.com>
2023-07-22 20:30:56 +0530

    Create Object.java

---
+++ New Java/Object.java
@@ -0,0 +1,10 @@
+package com.masai;
+public class Main{
+int x=10;
+public static void main(String[] args){
+int y=10;
+System.out.println(y);
+Main obj=new Main();
+System.out.println(obj.x);
+}
+}

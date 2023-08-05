42a9a3a1178cbb4520d0e588147a42cd7fdb3071
Rajat Tiwari <129544468+rajattiwariind@users.noreply.github.com>
2023-08-05 20:33:14 +0530

    Create Object3.java

---
+++ New Java/Object3.java
@@ -0,0 +1,12 @@
+public class MyClass {
+  int x;
+
+  public MyClass(int y) {
+   x = y;
+  }
+
+public static void main(String[] args) {
+  MyClass myObj = new MyClass(5);
+  System.out.println(myObj.x);
+  }
+}

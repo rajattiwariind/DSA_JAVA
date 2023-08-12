63dd8ae2b3bc3c1d4e78aeefb153320b51add295
Rajat Tiwari <129544468+rajattiwariind@users.noreply.github.com>
2023-08-12 19:02:11 +0530

    Create TwoDimen.java

---
+++ Array/TwoDimen.java
@@ -0,0 +1,14 @@
+public class TwoDimen {
+    public static void main(String []args){
+        int [] a={10,20,40};
+
+        int [][] twoDimen = {
+                {40,50,60,70},
+                {50,60,70,80},
+                {60,70,80,90}
+        };
+
+        int marks_0_0 = twoDimen[2][2];
+        System.out.println(marks_0_0);
+    }
+}

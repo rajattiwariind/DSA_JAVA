ce54f0d1fa264199032b598094f517029aafd596
Rajat Tiwari <129544468+rajattiwariind@users.noreply.github.com>
2023-08-15 18:29:57 +0530

    Create PersonTestDrive.java

---
+++ JAVA_BASIC/PersonTestDrive.java
@@ -0,0 +1,7 @@
+public class PersonTestDrive {
+    public static void main(String[] args) {
+      Person person= new Person();
+      person.setName("Aman");
+      System.out.println("Name of the person is " + person.getName());
+    }
+}

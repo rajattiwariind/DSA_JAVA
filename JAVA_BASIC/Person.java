38697c89f3e5c089662feed2f1d8506e1c5ac9ec
Rajat Tiwari <129544468+rajattiwariind@users.noreply.github.com>
2023-08-15 18:29:28 +0530

    Create Person.java

---
+++ JAVA_BASIC/Person.java
@@ -0,0 +1,13 @@
+public class Person {
+    private String name;
+    private int age;
+    private char gender;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}

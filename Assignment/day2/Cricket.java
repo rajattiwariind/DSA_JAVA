c9b0579c404b42d1470820a676dc55d979c02765
Rajat Tiwari <129544468+rajattiwariind@users.noreply.github.com>
2025-02-08 17:43:00 +0530

    Create Cricket.java

---
+++ Assignment/day2/Cricket.java
@@ -0,0 +1,13 @@
+public class Cricket {
+	public static void main(String[] args) {
+		int one=10;
+		int two=4;
+		int three=11;
+		int four=3;
+		int five=3;
+		int six=4;
+		int total_Score=0;
+		total_Score=(one+two+three+four+five+six);
+		System.out.println("Your total score is "+total_Score);
+	}
+}
